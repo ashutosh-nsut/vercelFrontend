08f353bc01f13b29b3bc944aaccf4755cc175916
Ashutosh Verma <ashutosh.verma.ug22@nsut.ac.in>
2025-06-10 22:41:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://vercel-backend-peach-one.vercel.app/"
+export const BASE_URL="https://vercel-backend-peach-one.vercel.app"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
